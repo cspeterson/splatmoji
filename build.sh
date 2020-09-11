62a5567f94c6af9a08a6dd66d54a7894b7cdea24
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2020-09-11 08:52:15 -0400

    Fix package file permissions by explicitly setting them in a script after install

--- build.sh
+++ build.sh
@@ -20,28 +20,32 @@ builddir="${SPLATMOJI_BUILDDIR:-./build}"
 rm -rf "${builddir:?}"/*
 
 # Docs
-mandir="${builddir}/usr/share/man/man1"
+realmandir="/usr/share/man/man1"
+mandir="${builddir}/${realmandir}"
 mkdir -p "${mandir}"
 pandoc --standalone --to man README.md -o "${mandir}/${cmdname}.1"
 
 # Bin
-bindir="${builddir}/usr/bin"
+realbindir="/usr/bin"
+bindir="${builddir}/${realbindir}"
 mkdir -p "${bindir}"
 cp "./${cmdname}" "${bindir}/"
-chmod 0755 "${bindir}/${cmdname}"
 
 # Config
-confdir="${builddir}/etc/xdg/${cmdname}"
+realconfdir="/etc/xdg/${cmdname}"
+confdir="${builddir}/${realconfdir}"
 mkdir -p "${confdir}"
 cp "./${cmdname}.config" "${confdir}/"
 
 # Lib
-libdir="${builddir}/usr/lib/${cmdname}"
+reallibdir="/usr/lib/${cmdname}"
+libdir="${builddir}/${reallibdir}"
 mkdir -p "${libdir}"
 cp  ./lib/* "${libdir}/"
 
 # Data
-datadir="${builddir}/usr/share/${cmdname}/data"
+realdatadir="/usr/share/${cmdname}"
+datadir="${builddir}/${realdatadir}/data"
 mkdir -p "${datadir}"
 cp -r ./data/* "${datadir}/"
 
@@ -54,7 +58,29 @@ elif command -v apt >/dev/null; then
 else
   echo 'Cannot determine target :(' 2>&1
 fi
+
+echo "#!/usr/bin/env bash
+  # This script creates the proper ownership/mode for Splatmoji's files after installation because I could not get fpm to do it by its own means :/
+
+  chown root:root '${realmandir}/${cmdname}.1'
+  chmod 0644 '${realmandir}/${cmdname}.1'
+
+  chown root:root '${realbindir}/${cmdname}'
+  chmod 0755 '${realbindir}/${cmdname}'
+
+  find '${realconfdir}' -exec chown root:root {} \;
+  find '${realconfdir}' -type f -exec chmod 0644 {} \;
+
+  find '${reallibdir}' -exec chown root:root {} \;
+  find '${reallibdir}' -type f -exec chmod 0644 {} \;
+
+  find '${realdatadir}' -exec chown root:root {} \;
+  find '${realdatadir}' -type f -exec chmod 0644 {} \;
+
+" > "${builddir}/splatmoji-postinstall-ownership.sh"
+
 fpm \
+  --after-install "${builddir}/splatmoji-postinstall-ownership.sh" \
   --architecture noarch \
   --depends 'bash > 4.4' \
   --depends 'rofi' \
@@ -68,5 +94,6 @@ fpm \
   --package "${builddir}/" \
   --url 'https://github.com/cspeterson/splatmoji' \
   --version "${version}" \
+  --verbose \
   \
   "${builddir}/"=/
